227dda1925a90b0b0977864904fa66b520712a0b
Jonathan Ramos <ramos.was.jonathan@gmail.com>
2019-09-24 00:22:59 -0300

    creating components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,16 @@ import { AngularFireModule } from '@angular/fire';
 import { environment } from 'src/environments/environment';
 import { AngularFireStorageModule } from '@angular/fire/storage';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { UploadFilesComponent } from './upload-files/upload-files.component';
+import { MyFilesComponent } from './my-files/my-files.component';
+import { DropzoneComponent } from './upload-files/dropzone/dropzone.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UploadFilesComponent,
+    MyFilesComponent,
+    DropzoneComponent
   ],
   imports: [
     BrowserModule,
